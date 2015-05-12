5c404f9aa7f4105c7c9031381f82be4f6861ffe6
Sehsyha <valentin.stern@telecomnancy.net>
2015-05-12 15:40:30 +0200

    Début du A étoile

--- astar.c
+++ astar.c
@@ -1,22 +1,40 @@
 #include "astar.h"
 
+int isEmptySet(int set[][], int row, int col);
 
 unsigned int nextDirection(int srcX, int srcY, int dstX, int dstY){
-    int diffX = srcX - dstX;
-    int diffY = srcY - dstY;
+    Map *map = getMapInstance();
+    unsigned int i, j;
     int direction = STATIC;
-    if(abs(diffX) > abs(diffY)){
-        if(diffX < 0){
-            direction = EAST;
-        }else{
-            direction = WEST;
-        }
-    }else{
-        if(diffY < 0){
-            direction = SOUTH;
-        }else{
-            direction = NORTH;
+
+    //TODO Verification des paramètres
+    int closedset[map->row][map->col];
+    for(i = 0 ; i  < map->row ; i++){
+        for(j = 0 ; j < map->col ; j++){
+            if(map->cells[i][j] == WALL){
+                closedset[i][j] = 1;
+            }
         }
     }
+
+    int openset[map->row][map->col];
+    openset[srcX][srcY] = 1;
+    while(isEmptySet(openset, map->row, map->col)){
+
+    }
     return direction;
+}
+
+
+int isEmptySet(int set[][], int row, int col){
+    int i, j, retour = 1;
+    for(i = 0 ; i < row ; i++){
+        for(j = 0 ; j < col ; j++){
+            if(set[i][j]){
+                retour = 0;
+                break;
+            }
+        }
+    }
+    return retour;
 }
