0cac9245ca6fb04c043ddedc735813d775959873
Switala <yoann.switala@telecomnancy.net>
2015-05-24 17:19:46 +0200

    odificationde certaine carte pour correspondre à la norme

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
 int main(void)
 {
     //Load the map
-    loadMap("../projec/hard.map");
+    loadMap("../projec/rdastn.map");
 
     //Search the pacman on the map and create him
     Pacman *pacman = getPacmanInstance();
