eacec1dc0533bfce4d7a0cc8a08eaa86324c86d4
Switala <yoann.switala@telecomnancy.net>
2015-05-24 11:27:18 +0200

    Modification A*

--- astar.c
+++ astar.c
@@ -1,40 +1,24 @@
 #include "astar.h"
+#include "ghost.h"
+#include "pacman.h"
 
-int isEmptySet(int set[][], int row, int col);
+float heuristique(Ghost *g,Pacman *p);
 
-unsigned int nextDirection(int srcX, int srcY, int dstX, int dstY){
+unsigned int nextDirection(Ghost *g,Pacman *p){
     Map *map = getMapInstance();
-    unsigned int i, j;
-    int direction = STATIC;
+    float distance = heuristique(*g,*p);
 
-    //TODO Verification des paramètres
-    int closedset[map->row][map->col];
-    for(i = 0 ; i  < map->row ; i++){
-        for(j = 0 ; j < map->col ; j++){
-            if(map->cells[i][j] == WALL){
-                closedset[i][j] = 1;
-            }
-        }
-    }
-
-    int openset[map->row][map->col];
-    openset[srcX][srcY] = 1;
-    while(isEmptySet(openset, map->row, map->col)){
-
-    }
     return direction;
 }
 
+float heuristique(Ghost *g,Pacman *p) {
+    int gx,gy,px,py;
+    float res;
+    gx = g->x;
+    gy = g->y;
+    px = p->x;
+    py = p->y;
+    res = (py-px)/(gy-gx);
 
-int isEmptySet(int set[][], int row, int col){
-    int i, j, retour = 1;
-    for(i = 0 ; i < row ; i++){
-        for(j = 0 ; j < col ; j++){
-            if(set[i][j]){
-                retour = 0;
-                break;
-            }
-        }
-    }
-    return retour;
+    return res;
 }
