74c91516147c1840862539362f5234883640e7ed
Switala <yoann.switala@telecomnancy.net>
2015-05-24 13:10:51 +0200

    Mise au point liste incidence

--- astar.c
+++ astar.c
@@ -1,6 +1,7 @@
 #include "astar.h"
 #include "ghost.h"
 #include "pacman.h"
+#include "liste_chainee.h"
 
 float heuristique(Ghost *g,Pacman *p);
 
@@ -28,7 +29,40 @@ Liste *graphageMap(Map *m) {
     c = m ->col;
     l = m ->row;
 
+    Liste rep[c][l];
+    for (i = 1;i < c - 1; i++) {
+        for (j = 1; j < l - 1; j++) {
+            Liste *incident = initialisation();
 
+            if (m ->cells[i][j] == 1) {
+                rep[i][j] = *incident;
+            } else {
+                if (m->cells[i-1][j] == 1) {
+                    insertion(incident,0);
+                } else {
+                    insertion(incident,1);
+                }
 
+                if (m->cells[i][j+1] == 1) {
+                    insertion(incident,0);
+                } else {
+                    insertion(incident,1);
+                }
 
+                if (m->cells[i+1][j] == 1) {
+                    insertion(incident,0);
+                } else {
+                    insertion(incident,1);
+                }
+
+                if (m->cells[i][j-1] == 1) {
+                    insertion(incident,0);
+                } else {
+                    insertion(incident,1);
+                }
+            }
+
+        }
+    }
+    return *rep;
 }
